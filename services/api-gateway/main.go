8066b1f39385559a456897fcc0dd6cb1a2584452
Peter Hajdu <55036998+Hajdudev@users.noreply.github.com>
2026-01-31 08:07:21 +0100

    Initial commit

---
+++ services/api-gateway/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"my-golang-blueprint/shared/env"
+	"net/http"
+)
+
+var httpAddr = env.GetString("HTTP_ADDR", ":8081")
+
+func main() {
+	log.Println("Starting API Gateway")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello from API Gateway"))
+	})
+
+	http.ListenAndServe(httpAddr, nil)
+}
